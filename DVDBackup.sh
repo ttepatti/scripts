d69cb375f41b95e96dc30d5f1fb6d67cbc69b2e1
Tim Tepatti <ttepatti@gmail.com>
2018-10-30 02:12:28 -0400

    Added DVD backup script
    yeet

---
+++ DVDBackup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# DVDBackup Script
+# By Tim
+
+# Insert your download path here
+backups="/srv/dev-disk-by-label-eggs/eggs/DVDs/"
+# dvdbackup command
+echo Running dvdbackup...
+dvdbackup -i /dev/sr0 -o $backups -M
+
+echo Running genisoimage...
+echo Getting name of backed-up DVD...
+fn=$(ls -t $backups | head -n1)
+genisoimage -dvd-video -udf -o "$backups$fn.iso" "$backups$fn/"
+
+echo DVD backup of $fn completed! Enjoy!

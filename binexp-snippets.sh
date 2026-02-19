ce871d486b81d2167bf070c5232eb8a6bd350cc0
Tim Tepatti <tim@tepatti.com>
2026-02-19 12:17:49 -0500

    created binexp-snippets.sh

---
+++ binexp-snippets.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# i always forget the useful little snippets for doing binary exploitation stuff, so this file exists to hold 'em as I remember 'em
+
+# print 100 A's
+printf 'A%.0s' {1..100}
